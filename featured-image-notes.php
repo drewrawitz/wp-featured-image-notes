01408f9e95fe17ffb44ff3d8c3c40b2dec653006
Drew Rawitz <email@drewrawitz.com>
2016-03-04 10:56:40 -0500

    initial commit

---
+++ featured-image-notes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Featured Image Notes
+ * Plugin URI: http://www.drewrawitz.com
+ * Description: This plugin allows you to add notes to the Featured Image box for certain posts / pages
+ * Version: 1.0.0
+ * Author: Drew Rawitz
+ * Author URI: http://www.drewrawitz.com
+ * License: GPL2
+ */
