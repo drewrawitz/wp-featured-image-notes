002a5e677e45f10b89180270e65e715bbf4f9f93
Drew Rawitz <email@drewrawitz.com>
2016-07-04 15:15:47 -0400

    Fixes Issue #1 where the uploaded thumbnail wasn't showing

--- public/class-featured-image-notes-public.php
+++ public/class-featured-image-notes-public.php
@@ -22,57 +22,59 @@
  */
 class Featured_Image_Notes_Public {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+  /**
+   * The ID of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $plugin_name    The ID of this plugin.
+   */
+  private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+  /**
+   * The version of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $version    The current version of this plugin.
+   */
+  private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+  /**
+   * Initialize the class and set its properties.
+   *
+   * @since    1.0.0
+   * @param      string    $plugin_name       The name of the plugin.
+   * @param      string    $version    The version of this plugin.
+   */
+  public function __construct( $plugin_name, $version ) {
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+    $this->plugin_name = $plugin_name;
+    $this->version = $version;
 
-	}
+  }
 
-	/**
-	 * Register the hooks
-	 *
-	 * @since    1.0.0
-	 */
-	public function add_hooks() {
+  /**
+   * Register the hooks
+   *
+   * @since    1.0.0
+   */
+  public function add_hooks() {
     add_filter('admin_post_thumbnail_html', 'featured_image_notes');
 
     function featured_image_notes($content) {
+      global $post;
+      $data = null;
       $options = get_option('featured-image-notes');
-      switch(get_post_type()) {
-        default:
-          foreach($options as $key => $value) :
-            if(get_post_type() === $key) :
-              $notes = $value;
-              return $notes . $content;
-            endif;
-          endforeach;
-        }
+
+      if($options[$post->post_type]) {
+        $data .= $options[$post->post_type];
       }
-	}
+
+      $data .= $content;
+
+      return $data;
+    }
+  }
 }
+
